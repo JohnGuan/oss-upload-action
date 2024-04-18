0a1795b0708412bef04624300c2314403bf003ee
John Guan <Guan.Ningchuan@gmail.com>
2024-04-18 13:53:33 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# OSS Action
+
+上传单个文件或文件夹所有文件到 OSS
+
+## Inputs
+
+- `key-id`: OSS AccessKeyId
+- `key-secret`: OSS AccessKeySecret
+- `region`: 区域，如 `oss-cn-shenzhen`，和 endpoint 二选一
+- `endpoint`: 优先级高于 region，可填写内网节点、加速节点，和 region 二选一
+- `bucket`: Bucket 名称
+- `assets`: 上传的资源。每行一条规则，格式：`源路径:目标路径`
+
+## Outputs
+
+- `url`: 文件在 OSS 上的 url。上传多个文件时，多个 url 用逗号隔开。
+
+## Usage
+
+```yaml
+- name: Upload to oss
+  id: upload_to_oss
+  uses: johnguan/oss-upload-action@master
+  with:
+    key-id: ${{ secrets.OSS_KEY_ID }}
+    key-secret: ${{ secrets.OSS_KEY_SECRET }}
+    region: oss-cn-shenzhen
+    bucket: tvrcgo
+    assets: |
+      a/**:/remote-a/
+      b/**:/remote-b/
+      c.txt:/rc.txt
+```
---
+++ src/index.js
@@ -0,0 +1,60 @@
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+const OSS = require('ali-oss');
+const fs = require('fs');
+const { resolve } = require('path');
+const fg = require('fast-glob');
+
+(async () => {
+  try {
+    // OSS 实例化
+    const opts = {
+      accessKeyId: core.getInput('key-id'),
+      accessKeySecret: core.getInput('key-secret'),
+      bucket: core.getInput('bucket')
+    }
+
+    ;['region', 'endpoint']
+      .filter(name => core.getInput(name))
+      .forEach(name => {
+        Object.assign(opts, {
+          [name]: core.getInput(name)
+        })
+      })
+
+    const oss = new OSS(opts)
+
+    // 上传资源
+    const assets = core.getInput('assets', { required: true })
+
+    assets.split('\n').forEach(async rule => {
+      const [src, dst] = rule.split(':')
+
+      const files = fg.sync([src], { dot: false, onlyFiles: true })
+
+      if (files.length && !/\/$/.test(dst)) {
+        // 单文件
+        const res = await oss.put(dst, resolve(files[0]))
+        core.setOutput('url', res.url)
+      } else if (files.length && /\/$/.test(dst)) {
+        // 目录
+        const res = await Promise.all(
+          files.map(async file => {
+            const base = src.replace(/\*+$/g, '')
+            const filename = file.replace(base, '')
+            return oss.put(`${dst}${filename}`, resolve(file), {
+              timeout: 1000*60*10
+            }).catch(err => {
+              core.setFailed(err && err.message)
+            })
+          })
+        )
+        core.setOutput('url', res.map(r => r.url).join(','))
+      }
+    })
+
+  } catch (err) {
+    core.setFailed(err.message)
+  }
+})()
